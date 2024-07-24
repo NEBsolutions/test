e11c96a91a9faf3836622893ed1d63de5b5477c6
pritam rana <pritamrana8848private@gmail.com>
2024-07-24 12:37:36 +0545

    hello  i am try to change cpp progrsm

---
+++ lalit.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class time
+{
+    int hours, minutes;
+
+public:
+    void gettime(int h, int m);
+    {
+        hours = h;
+        minutes = m;
+    }
+    void displaytime()
+    {
+        cout << hours << "hours and" << minutes << "minutes";
+    }
+    void sum(time, time);
+};
+void time::sum(t1, t2)
+
+    int main()
+{
+}
